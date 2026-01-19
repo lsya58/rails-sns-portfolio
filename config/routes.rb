eeb9db172aa82abadcc306580799bcd14717197b
lsya58 <milocbst369@gmail.com>
2026-01-19 17:34:04 +0900

    Add login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get  "/signup", to: "users#new"
-  post "/signup", to: "users#create"
+  root "sessions#new"
   resources :users
+  get    "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
 end
